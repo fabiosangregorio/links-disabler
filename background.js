f3c6b266af114b647f6c0c0744ee0ee22cb552b6
Fabio Sangregorio <fabio.sangregorio@gmail.com>
2018-10-24 22:33:14 +0200

    Finished extension implementation and added keyboard shortcut support

---
+++ background.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let linksDisabled = false;
+
+const disableLinks = () => {
+  [...document.getElementsByTagName('a')].forEach(link => {
+    link.setAttribute('data-linkdisabler-href', link.getAttribute('href'));
+    link.setAttribute('href', 'javascript:;');
+  });
+}
+
+const enableLinks = () => {
+  [...document.getElementsByTagName('a')].forEach(link => {
+    link.setAttribute('href', link.getAttribute('data-linkdisabler-href'));
+    link.removeAttribute('data-linkdisabler-href');
+  });
+}
+
+const executeScript = () => {
+  if (linksDisabled)
+    chrome.tabs.executeScript({
+      code: '(' + enableLinks + ')();'
+    });
+  else
+    chrome.tabs.executeScript({
+      code: '(' + disableLinks + ')();'
+    });
+
+  linksDisabled = !linksDisabled;
+}
+
+chrome.commands.onCommand.addListener(() => {
+  executeScript();
+});
+
+chrome.browserAction.onClicked.addListener(() => {
+  executeScript();
+});
